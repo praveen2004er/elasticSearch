3419ef35ab33dff571c775c9d8a40766824f5cba
praveen2004er <praveen2004er@gmail.com>
2019-05-04 16:37:30 +0530

    forming elastic search payload

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const elasticsearch = require('elasticsearch');
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const PORT = 5000;
 
@@ -7,6 +8,34 @@ const elasticSearchClient = new elasticsearch.Client({
     "host": "127.0.0.1:9200",
     "log":  "error"
 });
+
+const bulkIndex = function bulkIndex(index, type, data) {
+    let bulkBody = [];
+    data.forEach(element => {
+        bulkBody.push({
+            index: {
+                _index: index,
+                _type: type,
+                _id : element.Id
+            }
+        });
+
+        bulkBody.push(element);
+    });
+
+    console.log(bulkBody);
+}
+
+
+
+async function indexData() {
+    const rawReview = await fs.readFileSync('./sample_data.json');
+    const reviews = JSON.parse(rawReview);
+    bulkIndex('movie', 'reviews', reviews);
+
+}
+
+indexData();
 
 app.listen(PORT, function() {
     console.log("Server running successfully",  PORT);
